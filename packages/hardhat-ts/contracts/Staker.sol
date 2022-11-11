5c0b89121c3c6c6806624b421bc1a2af5595c0a9
0xBaconPancakes <0xBaconPancakes@gmail.com>
2022-11-11 13:19:06 +0100

    implement challenge 1

--- packages/hardhat-ts/contracts/Staker.sol
+++ packages/hardhat-ts/contracts/Staker.sol
@@ -1,25 +1,64 @@
-pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
 
 import 'hardhat/console.sol';
 import './ExampleExternalContract.sol';
 
 contract Staker {
-  ExampleExternalContract public exampleExternalContract;
+    ExampleExternalContract public exampleExternalContract;
+    mapping(address => uint256) public balances;
+    uint256 public constant threshold = 0.01 ether;
+    uint256 public deadline = block.timestamp + 72 hours;
+    bool public openForWithdraw = false;
 
-  constructor(address exampleExternalContractAddress) public {
-    exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+    constructor(address exampleExternalContractAddress) {
+        exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+    }
 
-  // TODO: Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+    // TODO: Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
+    //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+    event Stake(address indexed from, uint256 amount);
+    event OpenForWithdraw();
 
-  // TODO: After some `deadline` allow anyone to call an `execute()` function
-  //  It should call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+    modifier notCompleted() {
+        require(!exampleExternalContract.completed(), "Staker: already completed");
+        _;
+    }
 
-  // TODO: if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    function stake() public payable notCompleted {
+        require(timeLeft() > 0, "Staker: deadline passed");
+        balances[msg.sender] += msg.value;
+        emit Stake(msg.sender, msg.value);
+    }
 
-  // TODO: Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+    // TODO: After some `deadline` allow anyone to call an `execute()` function
+    //  It should call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value if the threshold is reached
+    function execute() public notCompleted {
+        require(timeLeft() == 0, 'Staker: too early');
+        if (address(this).balance >= threshold){
+            exampleExternalContract.complete{value : address(this).balance}();
+        } else {
+            openForWithdraw = true;
+            emit OpenForWithdraw();
+        }
+    }
 
-  // TODO: Add the `receive()` special function that receives eth and calls stake()
+    // TODO: if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    function withdraw() public notCompleted {
+        require(timeLeft() == 0, 'Staker: too early');
+        require(openForWithdraw, 'not open for withdraw');
+        uint256 amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+
+    // TODO: Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+    function timeLeft() public view returns (uint256) {
+        return deadline > block.timestamp ? deadline - block.timestamp : 0;
+    }
+
+    // TODO: Add the `receive()` special function that receives eth and calls stake()
+    receive() external payable {
+        stake();
+    }
 }
--- packages/vite-app-ts/src/models/constants/constants.ts
+++ packages/vite-app-ts/src/models/constants/constants.ts
@@ -1,8 +1,8 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = '3ccea23c26224daf802cf31e4699d15e';
+export const INFURA_ID = '6f142304895f4ee9b2ca4e71aeff76bf';
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
-export const ETHERSCAN_KEY = 'PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8';
+export const ETHERSCAN_KEY = '8TR2MGSFE5V8NGZUB713B5UEG4P14M4IZG';
 
 // BLOCKNATIVE ID FOR Notify.js:
 export const BLOCKNATIVE_DAPPID = '0b58206a-f3c0-4701-a62f-73c7243e8c77';
